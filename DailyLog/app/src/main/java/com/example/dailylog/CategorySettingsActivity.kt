29400a08eccf9b98b5bbe8d3da91866140344d88
Madeeha <madmfg@gmail.com>
2020-03-23 18:26:40 -0700

    Settings: manage Keyboard Shortcut and Category settings

---
+++ DailyLog/app/src/main/java/com/example/dailylog/CategorySettingsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.dailylog
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+
+class CategorySettingsActivity : AppCompatActivity() {
+    private lateinit var presenter: ShortcutListPresenter
+    private lateinit var view: ShortcutListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.generic_keyboard_shortcuts_setup)
+        view = ShortcutListView(findViewById<ConstraintLayout>(R.id.genericShortcutLayout))
+        presenter = ShortcutListPresenter(view, R.string.shortcuts, "categories", application)
+        view.initializeView(presenter)
+        view.renderView()
+    }
+}
