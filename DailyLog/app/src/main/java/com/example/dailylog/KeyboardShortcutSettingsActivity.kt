cc2e104c2c282e3861ae88025b4634b3d369ced9
madCode <madCode@users.noreply.github.com>
2020-03-23 18:26:40 -0700

    Settings: manage Keyboard Shortcut and Category settings

---
+++ DailyLog/app/src/main/java/com/example/dailylog/KeyboardShortcutSettingsActivity.kt
@@ -0,0 +1,27 @@
+package com.example.dailylog
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View.generateViewId
+import android.view.inputmethod.EditorInfo
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet.*
+
+
+class KeyboardShortcutSettingsActivity : AppCompatActivity() {
+    private lateinit var presenter: ShortcutListPresenter
+    private lateinit var view: ShortcutListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.generic_keyboard_shortcuts_setup)
+        view = ShortcutListView(findViewById<ConstraintLayout>(R.id.genericShortcutLayout))
+        presenter = ShortcutListPresenter(view, R.string.shortcuts, "global_shortcuts", application)
+        view.initializeView(presenter)
+        view.renderView()
+    }
+}
---
+++ DailyLog/app/src/main/java/com/example/dailylog/ShortcutListPresenter.kt
@@ -0,0 +1,35 @@
+package com.example.dailylog
+
+import android.app.Application
+
+class ShortcutListPresenter(private var view: ShortcutListView,
+                            private var titleResId: Int, private var preferencesKey: String,
+                            private var application: Application
+) {
+    private lateinit var shortcutList: ShortcutList
+
+    fun initializePresenter() {
+        this.view = view
+        shortcutList = ShortcutList(preferencesKey)
+        shortcutList.loadShortcuts(application)
+        view.setTitle(titleResId)
+        view.renderShortcutList(shortcutList.shortcutList)
+    }
+
+    fun addShortcut(shortcut: String): Boolean {
+        shortcutList.addShortcut(shortcut, application)
+        view.renderShortcutList(shortcutList.shortcutList)
+        return true
+    }
+
+    fun removeShortcut(shortcut: String): Boolean {
+        shortcutList.removeShortcut(shortcut, application)
+        view.renderShortcutList(shortcutList.shortcutList)
+        return true
+    }
+
+    interface View {
+        fun renderShortcutList(shortcutList: MutableSet<String>)
+        fun setTitle(title: Int)
+    }
+}
