01f124081f3eb94da7c2a2f6232c26aa4cd0db7d
madCode <madCode@users.noreply.github.com>
2020-03-30 02:55:06 -0700

    Show date stamp in new log

--- DailyLog/app/src/main/java/com/example/dailylog/AddToLogActivity.kt
+++ DailyLog/app/src/main/java/com/example/dailylog/AddToLogActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dailylog
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -11,15 +12,25 @@ class AddToLogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_to_log_screen)
-        val categoryShortcuts = ShortcutList("categories")
+        val categoryShortcuts = ShortcutList(Constants.CATEGORIES_LIST_PREF_KEY)
         categoryShortcuts.loadShortcuts(application)
-        val globalShortcuts = ShortcutList("global_shortcuts")
+        val globalShortcuts = ShortcutList(Constants.SHORTCUTS_LIST_PREF_KEY)
         globalShortcuts.loadShortcuts(application)
         view = AddToLogView(findViewById<ConstraintLayout>(R.id.addToLogView), applicationContext, categoryShortcuts, globalShortcuts)
 
         val fileHelper = FileHelper
         fileHelper.setUpHelper(application)
         presenter = AddToLogPresenter(applicationContext, FileHelper)
-        view.render(presenter)
+        view.render(presenter, getDateTimeFormat())
+    }
+
+    private fun getDateTimeFormat(): String {
+        val preferences =
+            application.getSharedPreferences(
+                application.applicationContext.getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE
+            )
+        return preferences.getString(
+            Constants.DATE_TIME_PREF_KEY, Constants.DATE_TIME_DEFAULT_FORMAT) ?: Constants.DATE_TIME_DEFAULT_FORMAT
     }
 }
