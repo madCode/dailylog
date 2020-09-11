15f7038f46b5e19a18be3584cb86f45f1223fff2
Madeeha Ghori <madeeha@sydney.local>
2020-09-11 00:36:46 -0700

    Fixing addtologactivity

--- DailyLog/app/src/main/java/com/example/dailylog/AddToLogActivity.kt
+++ DailyLog/app/src/main/java/com/example/dailylog/AddToLogActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dailylog
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -11,8 +12,25 @@ class AddToLogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_to_log_screen)
-        view = AddToLogView(findViewById<ConstraintLayout>(R.id.addToLogView), applicationContext)
-        presenter = AddToLogPresenter(applicationContext)
-        view.render(presenter)
+        val categoryShortcuts = ShortcutList(Constants.CATEGORIES_LIST_PREF_KEY)
+        categoryShortcuts.loadShortcuts(application)
+        val globalShortcuts = ShortcutList(Constants.SHORTCUTS_LIST_PREF_KEY)
+        globalShortcuts.loadShortcuts(application)
+        view = AddToLogView(findViewById<ConstraintLayout>(R.id.addToLogView), applicationContext, categoryShortcuts, globalShortcuts)
+
+        val fileHelper = FileHelper
+        fileHelper.setUpHelper(application)
+        presenter = AddToLogPresenter(applicationContext, fileHelper)
+        view.render(presenter, getDateTimeFormat())
+    }
+
+    private fun getDateTimeFormat(): String {
+        val preferences =
+            application.getSharedPreferences(
+                application.applicationContext.getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE
+            )
+        return preferences.getString(
+            Constants.DATE_TIME_PREF_KEY, Constants.DATE_TIME_DEFAULT_FORMAT) ?: Constants.DATE_TIME_DEFAULT_FORMAT
     }
 }
