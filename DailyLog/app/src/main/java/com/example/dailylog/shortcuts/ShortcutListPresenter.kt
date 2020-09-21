586837829a367661ad7e03cd5a9b24274916bd75
Madeeha Ghori <madeeha@sydney.local>
2020-09-20 17:36:09 -0700

    Make shortcut list in settings reorderable

--- DailyLog/app/src/main/java/com/example/dailylog/shortcuts/ShortcutListPresenter.kt
+++ DailyLog/app/src/main/java/com/example/dailylog/shortcuts/ShortcutListPresenter.kt
@@ -1,36 +1,52 @@
 package com.example.dailylog.shortcuts
 
 import android.app.Application
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dailylog.R
 
-class ShortcutListPresenter(private var view: ShortcutListView,
-                            private var titleResId: Int, private var descriptionResId: Int,
+class ShortcutListPresenter(private var view: android.view.View,
                             private var preferencesKey: String, private var application: Application
 ) {
     private lateinit var shortcutList: ShortcutList
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ShortcutListAdapter
 
     fun initializePresenter() {
         shortcutList = ShortcutList(preferencesKey, application)
         shortcutList.loadShortcuts()
-        view.setTitle(titleResId)
-        view.setDescription(descriptionResId)
-        view.renderShortcutList(shortcutList.labelList)
+        setUpShortcutList()
+        val addShortcutView = AddShortcutView(view) { label: String, text: String, cursor: Int ->
+            addShortcut(
+                label,
+                text,
+                cursor
+            )
+        }
+        addShortcutView.renderView()
+    }
+
+    private fun setUpShortcutList() {
+        adapter = ShortcutListAdapter(shortcutList.shortcutList) { label -> removeShortcut(label) }
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(application.applicationContext)
+
+        val callback: ItemTouchHelper.Callback = ShortcutTouchHelperCallback(adapter)
+        val shortcutTouchHelper = ItemTouchHelper(callback)
+        shortcutTouchHelper.attachToRecyclerView(recyclerView)
     }
 
     fun addShortcut(label: String, text: String, cursor: Int): Boolean {
         val added = shortcutList.addShortcut(label, text, cursor)
-        view.renderShortcutList(shortcutList.labelList)
+        adapter.updateItems(shortcutList.shortcutList)
         return added
     }
 
-    fun removeShortcut(shortcut: String): Boolean {
+    private fun removeShortcut(shortcut: String) {
         shortcutList.removeShortcut(shortcut)
-        view.renderShortcutList(shortcutList.labelList)
-        return true
-    }
-
-    interface View {
-        fun renderShortcutList(shortcutList: MutableSet<String>)
-        fun setTitle(title: Int)
-        fun setDescription(descriptionResId: Int)
     }
 }
