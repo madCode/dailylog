de80e0af7b2e837ae99ceb73150cf36b0c2aa586
madCode <madCode@users.noreply.github.com>
2023-11-11 17:58:49 -0800

    remove unused import

--- app/src/main/java/com/app/dailylog/ui/log/LogFragment.kt
+++ app/src/main/java/com/app/dailylog/ui/log/LogFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
